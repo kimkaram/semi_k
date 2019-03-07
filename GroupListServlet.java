f49946534b7ea75afd5a92bd5aba5198500962f1
kimkaram <47126446+kimkaram@users.noreply.github.com>
2019-03-08 03:05:07 +0900

    Update GroupListServlet.java
    수정1차

--- GroupListServlet.java
+++ GroupListServlet.java
@@ -12,19 +12,20 @@ import javax.servlet.http.HttpServletResponse;
 
 import group.model.service.GroupMgtService;
 import group.model.vo.GroupMgt;
-
+import group2.model.service.GroupService;
+import group2.model.vo.Group;
 
 /**
- * Servlet implementation class GMemberListServlet
+ * Servlet implementation class GroupListServlet
  */
-@WebServlet("/gslist")
-public class GMemberListServlet extends HttpServlet {
+@WebServlet("/glist")
+public class GroupListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public GMemberListServlet() {
+    public GroupListServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -33,28 +34,50 @@ public class GMemberListServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
 		request.setCharacterEncoding("utf-8");
 		response.setContentType("text/html; charset=utf-8");
+
+		int currentPage = 1;
 		
-		String userId = request.getParameter("idsearch");
+		String groupNo = request.getParameter("leader");
+		if(request.getParameter("page") != null) {
+			currentPage = Integer.parseInt(request.getParameter("page"));
+		}
 		
-		ArrayList<GroupMgt> list = new GroupMgtService().searchMember(userId);
-		System.out.println(list);
+		System.out.println("groupNo서블릿 : " + groupNo);
+		
+		int limit = 10;
+		
+		GroupMgtService gservice = new GroupMgtService();
+		int listCount = gservice.getListCount();
+		
+		ArrayList<GroupMgt> list = gservice.selectList(groupNo, currentPage, limit);
+		
+		int maxPage = (int)((double)listCount / limit + 0.9);
+		int startPage = (((int)((double)currentPage / limit + 0.9))
+				- 1) * limit + 1;
+		int endPage = startPage + limit - 1;
+		
+		if(maxPage < endPage)
+			endPage = maxPage;
+		
 		RequestDispatcher view = null;
-		if(list.size()  > 0) {
-			view = request.getRequestDispatcher("views/group/GMemberList.jsp");
-			request.setAttribute("list", list);
-			System.out.println("listS : " + list);
-			//request.setAttribute("group", group.setUserId(ss));
-			//System.out.println("group2 : " + group);
+		
+		if(list != null) {
+			view = request.getRequestDispatcher("views/group/leaderListView.jsp");
+			request.setAttribute("glist", list);
+			System.out.println("glist서블릿 : " + list);
+			request.setAttribute("currentPage", currentPage);
+			request.setAttribute("maxPage", maxPage);
+			request.setAttribute("startPage", startPage);
+			request.setAttribute("endPage", endPage);
+			request.setAttribute("listCount", listCount);
 			view.forward(request, response);
 		} else {
 			view = request.getRequestDispatcher("views/group/groupError.jsp");
-			request.setAttribute("message", "모임회원 전체 목록 조회 실패!");
+			request.setAttribute("message", currentPage + "에 대한 목록 조회 실패!");
 			view.forward(request, response);
 		}
-		
 	}
 
 	/**
